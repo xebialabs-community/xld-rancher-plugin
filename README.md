a62b5c4779a5883f2ef6a23b426ef650847e4d2a
droberts2013 <droberts@xebialabs.com>
2017-08-25 13:25:29 -0400

    Add transitioning/status check, improve documentation

--- README.md
+++ README.md
@@ -1,9 +1,44 @@
 # xld-rancher-plugin
 
-Configuration
+### Configuring the CLI interface ###
 
-Set up a rancher.rancherClient object with Rancher server URL, access key and secret key.
+Set up a rancher.RancherCliClient object with Rancher server URL, access key and secret key.
 
 Configure an application stack as a a compose.zip file containing a docker-compose.yml file and a rancher-compose.yml file.  Create a rancher.ComposeArchive artifact from the compose.zip file.
 
-Deploy the rancher.ComposeArtifact to the rancher.rancherClient in order to create the application stack on Rancher.
+![Screenshot of RancherCliClient](images/RancherCliClient.png)
+
+### Configuring the REST interface ###
+
+Define a rancher.RancherRestClient object in the Infrastructure tree.  It can be located under the root node or any folder.  Set the host (DNS or IP), port, access key, and secret key.
+
+![Screenshot of RancherRestClient](images/RancherRestClient.png)
+
+
+### Deploying ###
+
+
+Deploy the rancher.ComposeArtifact to the rancher.RancherCliClient or the rancher.RancherRestClient in order to create, update, upgrade, or delete the corresponding object on Rancher.
+
+##### Mapping of XL Deploy actions to Rancher actions #####
+
+**CLI**
+
+* CREATE -- implemented to create a stack
+
+* MODIFY
+
+* NOOP
+
+* DESTROY
+
+** REST **
+
+* CREATE
+
+* MODIFY
+
+* NOOP  -- implemented to upgrade a service
+
+* DESTROY
+
--- src/main/resources/rancher/rest/RancherClient.py
+++ src/main/resources/rancher/rest/RancherClient.py
@@ -1,11 +1,7 @@
 #
-# Copyright 2017 XEBIALABS
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#    THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+#    FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
 #
 
 import base64
@@ -19,45 +15,68 @@ from http.http_response import HttpResponse
 from http.http_entity_builder import HttpEntityBuilder
 
 class RancherClient( object ):
-   def __init__(self, url, username, password):
+    def __init__(self, url, username, password):
       self.url = url
       if url.endswith('/'):
-         self.url = url[:-1]
+          self.url = url[:-1]
       self.http_connection = HttpConnection(url, username, password)
       self.request = HttpRequest(self.http_connection, username, password)
-   # End __init__
+  # End __init__
 
-   def upgradeRancherServices(self, projectName, stackName, serviceName):
-      projectUrlString = 'v2-beta/projects?name=%s' % projectName
-      r = self.request.get(projectUrlString, contentType = 'application/json')
-      response = json.loads(r.response)
-      stacksLink = response['data'][0]['links']['stacks']
-      print "Stacks link is %s" % stacksLink
+    def upgradeRancherServices(self, projectName, stackName, serviceName):
+        projectUrlString = 'v2-beta/projects?name=%s' % projectName
+        r = self.request.get(projectUrlString, contentType = 'application/json')
+        response = json.loads(r.response)
+        stacksLink = response['data'][0]['links']['stacks']
+        print "Stacks link is %s" % stacksLink
 
-      stacksUrlString = "%s?name=%s" % (urlparse.urlparse(stacksLink).path[1:], stackName)
-      r = self.request.get(stacksUrlString, contentType = 'application/json')
-      response = json.loads(r.response)
-      servicesLink = response['data'][0]['links']['services']
-      print "Services link is %s" % servicesLink
+        stackUrlString = "%s?name=%s" % (urlparse.urlparse(stacksLink).path[1:], stackName)
+        r = self.request.get(stackUrlString, contentType = 'application/json')
+        response = json.loads(r.response)
+        servicesLink = response['data'][0]['links']['services']
+        print "Services link is %s" % servicesLink
 
-      serviceUrlString = "%s?name=%s" % (urlparse.urlparse(servicesLink).path[1:], serviceName)
-      r = self.request.get(serviceUrlString, contentType = 'application/json')
-      response = json.loads(r.response)
-      for service in response['data']:
-       print "%s:  %s" % (service['name'], service['state'])
-       selfLink = service['links']['self']
-       upgradeLink = service['actions']['upgrade']
-       print upgradeLink
-       print "service['upgrade'] = %s" % service['upgrade']
-       print "service['upgrade']['inServiceStrategy'] = %s" % service['upgrade']['inServiceStrategy']
+        serviceUrlString = "%s?name=%s" % (urlparse.urlparse(servicesLink).path[1:], serviceName)
+        r = self.request.get(serviceUrlString, contentType = 'application/json')
+        response = json.loads(r.response)
+        for service in response['data']:
+            print "%s:  %s" % (service['name'], service['state'])
+            selfLink = service['links']['self']
+            selfUrlString = urlparse.urlparse(selfLink).path[1:]
+            upgradeLink = service['actions']['upgrade']
+            print upgradeLink
+            print "service['upgrade'] = %s" % service['upgrade']
+            print "service['upgrade']['inServiceStrategy'] = %s" % service['upgrade']['inServiceStrategy']
        
-       upgradeConfig = service['upgrade']['inServiceStrategy']
-       upgradeRequestBody = {"inServiceStrategy":upgradeConfig,"toServiceStrategy": None}
+            upgradeConfig = service['upgrade']['inServiceStrategy']
+            upgradeRequestBody = {"inServiceStrategy":upgradeConfig,"toServiceStrategy": None}
        
-       upgradeUrlString = "%s?%s" % (urlparse.urlparse(upgradeLink).path[1:], urlparse.urlparse(upgradeLink).query)
-       print upgradeUrlString
-       r =self.request.post(upgradeUrlString, HttpEntityBuilder.create_string_entity(json.dumps(upgradeRequestBody)), contentType = 'application/json')
-       
-   # End upgradeRancherServices
+            upgradeUrlString = "%s?%s" % (urlparse.urlparse(upgradeLink).path[1:], urlparse.urlparse(upgradeLink).query)
+            print upgradeUrlString
+            r =self.request.post(upgradeUrlString, HttpEntityBuilder.create_string_entity(json.dumps(upgradeRequestBody)), contentType = 'application/json')
+        
+            if getStateAfterTransitioning(selfUrlString) == 'active':
+                print "Service is active"
+            else:
+                print "Service is not active; do rollback"
 
+
+    # End upgradeRancherServices
+
+    def getStateAfterTransitioning(serviceUrlString):
+        r = self.request.get(serviceUrlString, contentType = 'application/json')
+        response = json.loads(r.response)
+        serviceTransitioning = response['data'][0]['transitioning']
+        pollCount = 0
+        while serviceTransitioning != 'no':
+            time.sleep(10)
+            pollCount = pollCount + 1
+            Print "Transitioning, waiting... %d" % pollCount
+            r = self.request.get(serviceUrlString, contentType = 'application/json')
+            response = json.loads(r.response)
+            serviceTransitioning = response['data'][0]['transitioning']
+        r = self.request.get(serviceUrlString, contentType = 'application/json')
+        response = json.loads(r.response)
+        return response['data'][0]['state']
+        
 # End RancherClient
