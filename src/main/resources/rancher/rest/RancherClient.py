1aa725b5db36b67386878a418bafcc0adafc164a
droberts2013 <droberts@xebialabs.com>
2017-08-25 18:03:01 -0400

    Plugin-version.properties, Python fixes

--- src/main/resources/rancher/rest/RancherClient.py
+++ src/main/resources/rancher/rest/RancherClient.py
@@ -1,7 +1,11 @@
 #
-#    THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR
-#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
-#    FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
+# Copyright 2017 XEBIALABS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
 import base64
@@ -28,55 +32,58 @@ class RancherClient( object ):
         r = self.request.get(projectUrlString, contentType = 'application/json')
         response = json.loads(r.response)
         stacksLink = response['data'][0]['links']['stacks']
-        print "Stacks link is %s" % stacksLink
+        print "Stacks link is %s\n" % stacksLink
 
-        stackUrlString = "%s?name=%s" % (urlparse.urlparse(stacksLink).path[1:], stackName)
-        r = self.request.get(stackUrlString, contentType = 'application/json')
+        stacksUrlString = "%s?name=%s" % (urlparse.urlparse(stacksLink).path[1:], stackName)
+        r = self.request.get(stacksUrlString, contentType = 'application/json')
         response = json.loads(r.response)
         servicesLink = response['data'][0]['links']['services']
-        print "Services link is %s" % servicesLink
+        print "Services link is %s\n" % servicesLink
 
-        serviceUrlString = "%s?name=%s" % (urlparse.urlparse(servicesLink).path[1:], serviceName)
-        r = self.request.get(serviceUrlString, contentType = 'application/json')
+        servicesUrlString = "%s?name=%s" % (urlparse.urlparse(servicesLink).path[1:], serviceName)
+        r = self.request.get(servicesUrlString, contentType = 'application/json')
         response = json.loads(r.response)
         for service in response['data']:
-            print "%s:  %s" % (service['name'], service['state'])
+            print "%s:  %s\n" % (service['name'], service['state'])
             selfLink = service['links']['self']
             selfUrlString = urlparse.urlparse(selfLink).path[1:]
             upgradeLink = service['actions']['upgrade']
-            print upgradeLink
-            print "service['upgrade'] = %s" % service['upgrade']
-            print "service['upgrade']['inServiceStrategy'] = %s" % service['upgrade']['inServiceStrategy']
-       
-            upgradeConfig = service['upgrade']['inServiceStrategy']
+            print "Upgrading with upgradeLink %s\n" % upgradeLink
+            # print "service['upgrade'] = %s" % service['upgrade']
+            # print "service['upgrade']['inServiceStrategy'] = %s" % service['upgrade']['inServiceStrategy']
+
+            if service['upgrade']:       
+                upgradeConfig = service['upgrade']['inServiceStrategy']
+            else:
+                upgradeConfig = None
+
             upgradeRequestBody = {"inServiceStrategy":upgradeConfig,"toServiceStrategy": None}
        
             upgradeUrlString = "%s?%s" % (urlparse.urlparse(upgradeLink).path[1:], urlparse.urlparse(upgradeLink).query)
             print upgradeUrlString
             r =self.request.post(upgradeUrlString, HttpEntityBuilder.create_string_entity(json.dumps(upgradeRequestBody)), contentType = 'application/json')
         
-            if getStateAfterTransitioning(selfUrlString) == 'active':
+            if self.getStateAfterTransitioning(selfUrlString) == 'active':
                 print "Service is active"
             else:
-                print "Service is not active; do rollback"
-
+                print "Service is not active; do rollback\n"
 
     # End upgradeRancherServices
 
-    def getStateAfterTransitioning(serviceUrlString):
+    def getStateAfterTransitioning(self, serviceUrlString):
         r = self.request.get(serviceUrlString, contentType = 'application/json')
         response = json.loads(r.response)
-        serviceTransitioning = response['data'][0]['transitioning']
+        serviceTransitioning = response['transitioning']
         pollCount = 0
         while serviceTransitioning != 'no':
             time.sleep(10)
             pollCount = pollCount + 1
-            Print "Transitioning, waiting... %d" % pollCount
+            print "Transitioning, waiting... %d\n" % pollCount
             r = self.request.get(serviceUrlString, contentType = 'application/json')
             response = json.loads(r.response)
-            serviceTransitioning = response['data'][0]['transitioning']
+            serviceTransitioning = response['transitioning']
         r = self.request.get(serviceUrlString, contentType = 'application/json')
         response = json.loads(r.response)
-        return response['data'][0]['state']
+        return response['state']
         
 # End RancherClient
